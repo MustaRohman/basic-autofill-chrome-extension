0f66f11237a2d89125451cc1f0ea6f2c9a3e7e70
Musta Rohman <mustarohman@gmail.com>
2016-07-18 01:25:53 +0100

    Now whenever the page loads a dialog appears asking to autofill the form. If yes, the form is filled.This needs to be limited to urls that are stored in local storage

--- content-script.js
+++ content-script.js
@@ -1,11 +1,20 @@
-
-var userNameInput = $("input[type='email']");
-console.log("Number of text inputs: ", userNameInput.length);
-if (userNameInput.length === 0) {
-	userNameInput = $("input[type='text']");
+/**
+ * @param  {String} Uses URL string to 
+ * @return {[type]}
+ */
+function fillForm(url) {
+    var userNameInput = $("input[type='email']");
+    console.log("Number of text inputs: ", userNameInput.length);
+    if (userNameInput.length === 0) {
+        userNameInput = $("input[type='text']");
+    }
+    userNameInput.first().val('MustaRohman');
+    $("input[type='password']").first().val('Random');
 }
-userNameInput.first().val('MustaRohman');
-$("input[type='password']").first().val('Random');
 
+var url = window.location.href;
 
-console.log("Web page is ready!");
+if (confirm("Fill in form?")){
+  fillForm("url");
+}
+

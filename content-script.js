fde128992a93da168216d979047890f17e8f48fd
Musta Rohman <mustarohman@gmail.com>
2016-07-20 21:48:34 +0100

    urls are now stored using chrome.storage. Code has been refactored in this way because chrome.storage.get method is asynchronous, so I had to utilise the callback function. Still need to change the code in popup.js in same way

--- content-script.js
+++ content-script.js
@@ -1,4 +1,23 @@
 /**
+ * @param  {[type]} Url String
+ * @return {Boolean} Yes, if URL is stored in local storage
+ */
+function isUrlStored(storedUrls, pageURL) {
+  console.log(pageUrl, ": Current page url. No. of stored URLS: ", storedUrls.length);
+  var value = false;
+    for (var i = 0; i < storedUrls.length; i++) {
+        var currentURL = urls[i];
+        console.log(currentURL, ": url at ", i);
+        if (currentURL.trim() === pageUrl.trim()){
+          value = true;
+          break;
+        }   
+    }
+    return value;
+}
+
+//Add event listener to submit button
+/**
  * @param  {String} Uses URL string to 
  * @return {[type]}
  */
@@ -12,9 +31,30 @@ function fillForm(url) {
     $("input[type='password']").first().val('Random');
 }
 
-var url = window.location.href;
+var pageUrl = window.location.href;
+console.log(pageUrl.trim());
+var value = false;
 
-if (confirm("Fill in form?")){
-  fillForm("url");
-}
+chrome.storage.sync.get('urls', function (items) {
+    urls = items.urls;
+    if (isUrlStored(urls, pageUrl)){
+      console.log("url has been found");
+          if (confirm("Fill in form?")){
+            fillForm("url");
+            console.log("Form has been filled");
+          } else {
+            console.log("form filling cancelled")
+          }
+    }
+  }); 
 
+// var url = window.location.href;
+// console.log(url.trim());
+// if (isUrlStored(url)){
+//   if (confirm("Fill in form?")){
+//     fillForm("url");
+//     console.log("Form has been filled");
+//   }
+// } else {
+//   console.log(url, " not found")
+// }

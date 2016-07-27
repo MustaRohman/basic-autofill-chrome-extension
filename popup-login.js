0d6d60880ffe3461b654dbecc93eccba19c10615
Musta Rohman <mustarohman@gmail.com>
2016-07-28 00:54:55 +0100

    Added a 'Forgot password?' link in popup-login.html. In popup-login.js, the submit button now goes to the original popup.html. An empty function called checkLoginRedentials has been added

--- popup-login.js
+++ popup-login.js
@@ -1,8 +1,16 @@
+function checkLoginCredentials() {
+    // body...
+}
+
 
 $(document).ready(function () {
-	var submitBtn = $("#submit");
-	console.log(submitBtn.length);
-	submitBtn.click(function () {
-		console.log("Testing Submit button");
-	});
-})
+    var submitBtn = $("#submit");
+    console.log(submitBtn.length);
+    submitBtn.click(function () {
+        console.log("Testing Submit button");
+        checkLoginCredentials();
+        chrome.browserAction.setPopup({popup: "popup.html"});
+        window.location.href = "popup.html";
+    });
+});
+

27a9b3b60a08ef6a423915b4fb38059d8ac16746
Musta Rohman <mustarohman@gmail.com>
2016-07-29 22:13:22 +0100

    Added functions that allows list-items to be added programmatically. These list items are now selectable and will deselect other items

--- settings.js
+++ settings.js
@@ -1,11 +1,31 @@
-$(document).ready(function () {
+function createListGroupItem(username, websiteName) {
 	console.log("Adding new list item...");
 	var $aElem = $('<a></a>');
-	$aElem.addClass("list-group-item");
+	$aElem.toggleClass("list-group-item");
+	
 	var $h4Elem = $('<h4></h4>');
-	$h4Elem.text("Github"); 
+	$h4Elem.addClass("list-group-item-heading")
+	$h4Elem.text(websiteName); 
+
 	var $pElem = $('<p></p>');
-	$pElem.text("mustarohman@gmail.com");
+	$pElem.text(username);
+	$pElem.addClass("list-group-item-text");
+
 	$aElem.append([$h4Elem, $pElem]);
+	$aElem.click(function () {
+		console.log("Number of items: ", $(".active").length);
+		$(".active").toggleClass("active");
+		$(this).toggleClass("active");
+	})
 	$(".list-group").append($aElem);
-})
+}
+
+function createListGroup(urlList) {
+	for (var i = 5; i >= 0; i--) {
+		createListGroupItem("mustarohman@gmail.com", "Github");
+	}
+}
+
+$(document).ready(function () {
+	createListGroup("urlList")
+});
